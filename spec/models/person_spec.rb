41be97a703a8c747eaf633792ca0c0b7b13fb23f
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-18 23:28:18 +0530

    added testing data for last name, if nil then invalid

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+
   it 'is invalid without a first name' do
   	person = Person.new(first_name: nil)
   	expect(person).not_to be_valid
   end
+
+  it 'is invalid without a last name' do
+  	person = Person.new(first_name: 'Bob', last_name: nil)
+  	expect(person).not_to be_valid
+  end
+
 end
