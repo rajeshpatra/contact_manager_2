db37a6363cdde2d2697c03a0ea59108b35c24350
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-18 19:07:52 +0530

    added to serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.serve_static_assests = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
