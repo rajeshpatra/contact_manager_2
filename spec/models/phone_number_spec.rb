d7d6fc08071d97d4b0008dc8091b361956160415
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-19 19:26:53 +0530

    checking phone number can't be blank

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) {PhoneNumber.new}
+
+  it 'is valid' do
+  	expect(phone_number).to be_valid
+  end
+
+  # Phone number can't be blank
+  it 'is invalid without a number' do
+  	phone_number.number = nil
+  	expect(phone_number).not_to be_valid
+  end
+
+
 end
