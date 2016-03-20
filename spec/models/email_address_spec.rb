3f85f0b325e236de49d7ed077e0d989f6875e08f
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-20 20:12:50 +0530

    added a let block named email_address that returns EmailAddress.new

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:eamil_address) do
+  	EmailAddress.new
+  end
 end
