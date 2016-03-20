bce07a173a28b165d285657f062e654e213b7848
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-20 20:30:04 +0530

    updated let block giving an EmailAddress and address

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
   let(:eamil_address) do
-  	EmailAddress.new
+  	EmailAddress.new(address: 'you@email.com', person_id: 1)
   end
 
   it 'is valid' do
