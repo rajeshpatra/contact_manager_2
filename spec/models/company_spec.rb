7af6d2acc1beea22cefd5f63fc7394f35f8fa02d
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-21 08:14:40 +0530

    added a test to ensure that company is invalid without a name

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Company, type: :model do
   it 'is valid' do
   	expect(company).to be_valid
   end
+
+  it 'is invalid without a name' do
+  	company.name = nil
+  	expect(company).to_not be_valid
+  end
 end
