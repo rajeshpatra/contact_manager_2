c5600a8a658ddb4da5cfb4d6443f903f473e9a2e
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-21 08:19:00 +0530

    added validation of controller spec adding valid and invalid attributes

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe CompaniesController, type: :controller do
   # Company. As you add validations to Company, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {name: 'MyString'}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {name: nil}
   }
 
   # This should return the minimal set of values that should be in the session
