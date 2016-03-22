defe47e2f8c25f50d36f5add12d995e2383b924b
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-22 17:52:17 +0530

    some minor changes and deleted extraneous spec file

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,4 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
 	validates :number, :person_id, presence: true
 	belongs_to :person
+	# belongs_to :company
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe Company, type: :model do
   	company.name = nil
   	expect(company).to_not be_valid
   end
+
+  # it 'has an array phone numbers' do
+  # 	expect(company.phone_numbers).to eq ([])
+  # end
 end
