f6e269b664d7ad634cdffb6a7e5868dfa1e800d5
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-27 15:23:01 +0530

    added check for empty arrays and the relationship that they should

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -14,7 +14,14 @@ RSpec.describe Company, type: :model do
   	expect(company).to_not be_valid
   end
 
-  # it 'has an array phone numbers' do
+  # it 'has an array phone of numbers' do
   # 	expect(company.phone_numbers).to eq ([])
   # end
+
+
+  # below is replaced with 'has an array of phone numbers'
+  it 'responds with its phone number after they\'re created' do
+    phone_number = company.phone_numbers.build(number: '333-4444')
+    expect(phone_number.number).to eq('333-4444')
+  end
 end
