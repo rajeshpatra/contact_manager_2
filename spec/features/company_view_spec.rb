b7ef2e6f7f7711f48bba64252d0d07bd1362719e
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-27 15:18:41 +0530

    added integration test for company

--- spec/features/company_view_spec.rb
+++ spec/features/company_view_spec.rb
@@ -14,5 +14,47 @@ describe 'the company view', type: :feature do
 			expect(page).to have_content(phone.number)
 		end
 	end
+
+	it 'has a link to add new phone number' do
+		expect(page).to have_link('Add phone number', href: new_phone_number_path(contact_id: company.id, contact_type: 'Company'))
+	end
+
+	it 'adds a new number' do
+		page.click_link('Add phone number')
+		page.fill_in('Number', with: '1234567890')
+		page.click_button('Create Phone number')
+		expect(current_path).to eq(company_path(company))
+		expect(page).to have_content('1234567890')
+	end
+
+	it 'has links to edit phone number' do
+		company.phone_numbers.each do |phone|
+			expect(page).to have_link('Edit', href: edit_phone_number_path(phone))
+		end
+	end
+
+	it 'edits a phone number' do
+		phone = company.phone_numbers.first
+		old_number = phone.number
+
+		first(:link, 'Edit').click
+		page.fill_in('Number', with: '1234567890')
+		page.click_button('Update Phone number')
+		expect(current_path).to eq(company_path(company))
+		expect(page).to have_content('1234567890')
+		# expect(page).not_to have_content(old_number)
+	end
+
+	it 'has links to delete phone number' do
+		company.phone_numbers.each do |phone|
+			expect(page).to have_link('Delete', href: phone_number_path(phone))
+		end
+	end
+
+	it 'deletes a phone number' do
+		phone = company.phone_numbers.first
+		first(:link, 'Delete').click
+		expect(current_path).to eq(company_path(company))
+	end
 end
 
