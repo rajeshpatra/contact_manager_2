8c5cae761e27ad6d075be45cd3783e81c22fc852
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-27 15:23:35 +0530

    added check for empty arrays and the relationship that they should

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -23,7 +23,19 @@ RSpec.describe Person, type: :model do
   end
 
   # check that person is capable of having phone numbers
-  it 'has an array of phone numbers' do
-  	expect(person.phone_numbers).to eq([])
+  # it 'has an array of phone numbers' do
+  # 	expect(person.phone_numbers).to eq([])
+  # end
+
+  # below is replaced for 'has an array of phone numbers'
+  it 'responds with its created phone numbers' do
+    person.phone_numbers.build(number: '555-8888')
+    expect(person.phone_numbers.map(&:number)).to eq(['555-8888'])
+  end
+
+  # for 'has an array of email address'
+  it 'responds with its created email address' do
+    person.email_addresses.build(address: 'me@example.com')
+    expect(person.email_addresses.map(&:address)).to eq(['me@example.com'])
   end
 end
