3d3ed29922977b46c1f23b0593a91498a50ebb26
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-27 11:50:41 +0530

    added polymorphic relationship with contact

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,6 +1,7 @@
 class PhoneNumber < ActiveRecord::Base
 	validates :number, :contact_id, presence: true
 	# belongs_to :person
+	# :person changed to contact
 	belongs_to :contact, polymorphic: true
 	# belongs_to :company
 end
