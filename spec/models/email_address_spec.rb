80a17752229ab3080e723f91c8f706fde2265801
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-28 23:59:50 +0530

    some minor changes

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
+  let(:person) {Person.create(first_name: 'Amitabh', last_name: 'Pal')}
   let(:email_address) do
-  	EmailAddress.new(address: 'you@email.com', person_id: 1)
+  	EmailAddress.new(address: 'you@email.com', contact_id: person.id, contact_type: 'Person')
   end
 
   it 'is valid' do
@@ -15,11 +16,11 @@ RSpec.describe EmailAddress, type: :model do
   end
 
   it 'must have a reference to a person' do
-  	email_address.person_id = nil
+  	email_address.contact_id = nil
   	expect(email_address).not_to be_valid
   end
 
-  it 'is associated with person' do
-  	expect(email_address).to respond_to(:person)
+  it 'is associated with person or contact' do
+  	expect(email_address).to respond_to(:contact)
   end
 end
