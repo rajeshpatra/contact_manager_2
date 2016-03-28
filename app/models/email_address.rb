ab4b4bbcc391e7db78762f1e1e3a519b63d10caf
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-28 23:52:01 +0530

    added polymorphic relationship and contact_type

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,4 +1,4 @@
 class EmailAddress < ActiveRecord::Base
-	validates :address, :contact_id, presence: true
-	belongs_to :contact
+	validates :address, :contact_id, :contact_type, presence: true
+	belongs_to :contact, polymorphic: true
 end
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,5 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
-	validates :number, :contact_id, presence: true
+	validates :number, :contact_id, :contact_type, presence: true
 	# belongs_to :person
 	# :person changed to contact
 	belongs_to :contact, polymorphic: true
